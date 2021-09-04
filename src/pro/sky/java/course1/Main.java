5d2cb97d2e346b4ce43075c60df45d91825012f8
homework <9874@bk.ru>
2021-09-04 20:21:51 +0300

    home

---
+++ src/pro/sky/java/course1/Main.java
@@ -0,0 +1,15 @@
+package pro.sky.java.course1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Задание 1
+    // Задание 2
+    // Задание 3
+    // Задание 4
+    // Задание 5
+    // Задание 6
+    // Задание 7
+    // Задание 8
+    }
+}
